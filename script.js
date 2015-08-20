0ad118b5f81057588e17001e6bd87b688f0368db
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-08-20 20:36:04 -0300

    added function placeholder

--- script.js
+++ script.js
@@ -6,6 +6,42 @@ require([
   'dojo/dom-style',
   'dojo/domReady!'
 ], function( domAttr, query, array, on, domStyle ){
+
+  function placeholderCont( elemento ){
+
+    query( elemento ).forEach(function( node, index, arr ){
+      var
+        cor_elemento = domStyle.get( node, 'color' ),
+        valor_placeholder = domAttr.get( node, 'placeholder' ),
+        valor_input = domAttr.get( node, 'value' );
+        
+      if( valor_input === "" ){
+        domAttr.set( node, 'value', valor_placeholder );
+        domStyle.set( node, 'color', '#ccc' );
+      } else if( valor_input === valor_placeholder){
+          domStyle.set( node, 'color', '#ccc' );
+      }
+
+      on( node , 'focus', function() {
+        valor_placeholder = domAttr.get( this, 'placeholder');
+        
+        if ( domAttr.get( this, 'value' ) === valor_placeholder ){
+          domAttr.set( this, 'value', '' );
+          domStyle.set( this, 'color', cor_elemento );
+        }
+      });
+
+      on( node, 'blur', function() {
+        valor_placeholder = domAttr.get( this, 'placeholder' );
+        
+        if( domAttr.get( this, 'value' ) === "" ){
+          domAttr.set( this, 'value', valor_placeholder );
+          domStyle.set( this, 'color', '#ccc' );
+        }
+      });
+    });
+  }
+
   /**
    * @method verificaSuporteHtml5
    * Verifica suporta para html5
