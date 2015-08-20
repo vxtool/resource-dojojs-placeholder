4203c7787e7eb93c2c9cb2f54b7f60019aeee74a
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-08-20 20:32:37 -0300

    added base script

---
+++ script.js
@@ -0,0 +1,10 @@
+require([
+  'dojo/dom-attr',
+  'dojo/query',
+  'dojo/_base/array',
+  'dojo/on',
+  'dojo/dom-style',
+  'dojo/domReady!'
+], function( domAttr, query, array, on, domStyle ){
+
+});
