6e1198221070a50b7dbf5b72e1791e3a12114b7c
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-08-20 20:35:07 -0300

    doc of the function support html5

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ require([
   'dojo/dom-style',
   'dojo/domReady!'
 ], function( domAttr, query, array, on, domStyle ){
-
+  /**
+   * @method verificaSuporteHtml5
+   * Verifica suporta para html5
+   *
+   * @param {elemento} String com a identificação do elemento
+   * @param {atributo} String com o atributo do elemento
+   *     
+  */
   function verificaSuporteHtml5( elemento, atributo ){
     if( !( elemento && typeof elemento === 'string' ) ) return;
     if( !( atributo && typeof atributo === 'string' ) ) return;
