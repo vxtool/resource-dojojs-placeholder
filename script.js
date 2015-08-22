48d902f6422a7115628b77327fe3183d3a62ff24
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-08-22 20:48:13 -0300

    changing language

--- script.js
+++ script.js
@@ -11,23 +11,23 @@ require([
    * @method placeholderCont
    * Executa a função de placeholder alternativo para navegadores antigos
    *
-   * @param {elemento} String com a identificação do elemento
+   * @param {element} String com a identificação do elemento
    *     
   */
-  function placeholderCont( elemento ){
+  function placeholderCont( element ){
 
-    query( elemento ).forEach(function( node, index, arr ){
+    query( element ).forEach(function( node, index, arr ){
       var
         /**
          * @property {string} (RGB)
          * Propriedade com a cor original do elemento
         */
-        cor_elemento = domStyle.get( node, 'color' ),
+        colorElement = domStyle.get( node, 'color' ),
         /**
          * @property {string}
          * Propriedade com o valor do atributo 'placeholder' do elemento
         */
-        valor_placeholder = domAttr.get( node, 'placeholder' ),
+        valuePlaceholder = domAttr.get( node, 'placeholder' ),
         /**
          * @property {string}
          * Propriedade com o valor do atributo 'value' do elemento
@@ -36,28 +36,28 @@ require([
         
       // sendo o 'value' do elemento vazio, o mesmo recebe o valor do 'placeholder' e a cor cinza para escrita
       if( valor_input === "" ){
-        domAttr.set( node, 'value', valor_placeholder );
+        domAttr.set( node, 'value', valuePlaceholder );
         domStyle.set( node, 'color', '#ccc' );
-      } else if( valor_input === valor_placeholder){ //caso tenha refresh na pagina sem acionar os inputs
+      } else if( valor_input === valuePlaceholder){ //caso tenha refresh na pagina sem acionar os inputs
           domStyle.set( node, 'color', '#ccc' );
       }
 
       // o valor do 'placeholder' sendo igual ao do 'value', o 'value' recebe vazio e a cor original de escrita
       on( node , 'focus', function() {
-        valor_placeholder = domAttr.get( this, 'placeholder');
+        valuePlaceholder = domAttr.get( this, 'placeholder');
         
-        if ( domAttr.get( this, 'value' ) === valor_placeholder ){
+        if ( domAttr.get( this, 'value' ) === valuePlaceholder ){
           domAttr.set( this, 'value', '' );
-          domStyle.set( this, 'color', cor_elemento );
+          domStyle.set( this, 'color', colorElement );
         }
       });
 
       // o valor do 'value' sendo igual a vazio, o 'value' recebe o valor do 'placeholder' e a cor cinza para escrita
       on( node, 'blur', function() {
-        valor_placeholder = domAttr.get( this, 'placeholder' );
+        valuePlaceholder = domAttr.get( this, 'placeholder' );
         
         if( domAttr.get( this, 'value' ) === "" ){
-          domAttr.set( this, 'value', valor_placeholder );
+          domAttr.set( this, 'value', valuePlaceholder );
           domStyle.set( this, 'color', '#ccc' );
         }
       });
@@ -65,18 +65,18 @@ require([
   }
 
   /**
-   * @method verificaSuporteHtml5
+   * @method checkSupportHtml5
    * Verifica suporta para html5
    *
-   * @param {elemento} String com a identificação do elemento
-   * @param {atributo} String com o atributo do elemento
+   * @param {element} String com a identificação do elemento
+   * @param {attribute} String com o atributo do elemento
    *     
   */
-  function verificaSuporteHtml5( elemento, atributo ){
-    if( !( elemento && typeof elemento === 'string' ) ) return;
-    if( !( atributo && typeof atributo === 'string' ) ) return;
-    if( atributo in document.createElement( elemento ) ) return true;
+  function checkSupportHtml5( element, attribute ){
+    if( !( element && typeof element === 'string' ) ) return;
+    if( !( attribute && typeof attribute === 'string' ) ) return;
+    if( attribute in document.createElement( element ) ) return true;
   };
 
-  if( !verificaSuporteHtml5('input', 'valor_placeholder') ) placeholderCont( '.inp_text' );
+  if( !checkSupportHtml5('input', 'valor_placeholder') ) placeholderCont( '.inp_text' );
 });
